64f2985da922c14ddad6f3fc58b9a9354b2cf2d0
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 15:31:16 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.title("Générateur d'images Dall-e")
+st.title("Générateur d'images Dall-e 3")
+
+st.text("Entrez votre prompt ici :")
+prompt = st.input
+
+from openai import OpenAI
+client = OpenAI(api_key=OpenAIKEY)
+
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=prompt,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+
+image_url = image.data[0].url
+print(image_url)
+
+st.image("image_url")

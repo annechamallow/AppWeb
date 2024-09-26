9009c0962430d0589705b8626f8b60d169a99a94
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 14:41:58 +0200

    Update home.py

--- home.py
+++ home.py
@@ -22,3 +22,8 @@ student_grade = st.selectbox("Sélectionnez votre niveau d'études :", ["Bac","B
 
 # Select slider
 age = st.select_slider("Indiquez votre âge", range(0,99))
+
+if age > 18:
+  st.write("Vous êtes majeur")
+else
+  st.write("Vous êtes mineur") 

5b49e31445fc979b987c456f3bdd515fe451ae5c
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 14:22:17 +0200

    Update home.py

--- home.py
+++ home.py
@@ -8,5 +8,7 @@ st.write("Voici un petit formulaire de contact à remplir :")
 
 # Champ de saisie
 user_input = st.text_input("Entrez votre texte :")
-
 st.write(user_input)
+
+# image
+st.image("https://tempo.cdn.tambourine.com/windsong/media/bmot-website-homepage-header-desktop-65f9c01c2da38.mp4")

e02929ecc5b12033fe02f62e9fcf322e4a1db454
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 16:03:14 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -7,19 +7,17 @@ prompt_image = st.text_input("Entrez votre prompt ici :")
 
 openai_key = st.sidebar.text_input("Collez ici votre clé OpenAI :", '')
 
-try: 
-    client = OpenAI(api_key=openai_key)
+
+client = OpenAI(api_key=openai_key)
+
+image = client.images.generate(
+    model="dall-e-3",
+    prompt= prompt_image,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+
+image_url = image.data[0].url
     
-    image = client.images.generate(
-        model="dall-e-3",
-        prompt= prompt_image,
-        size="512x512",
-        quality="standard",
-        n=1,
-    )
-    
-    image_url = image.data[0].url
-    
-    #st.image(image_url)
-except:
-    pass
+st.image(image_url)

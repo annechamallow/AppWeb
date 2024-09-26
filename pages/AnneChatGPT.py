a14e6ea9d4bcc0143988205055f3229963dd596d
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 16:19:24 +0200

    Update AnneChatGPT.py

--- pages/AnneChatGPT.py
+++ pages/AnneChatGPT.py
@@ -9,13 +9,13 @@ client = OpenAI(api_key=openaikey)
 prompt = st.text_input("Tapez votre prompt : ")
 
 chat_completion = client.chat.completions.create(
-  message=prompt,
-  model="gpt-3.5-turbo",
-  temperature=0.3,
-  max_tokens=100,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-)
+      message=prompt,
+      model="gpt-3.5-turbo",
+      temperature=0.3,
+      max_tokens=100,
+      top_p=1.0,
+      frequency_penalty=0.0,
+      presence_penalty=0.0
+    )
 
 st.write(chat_completion.choices[0].message.content)

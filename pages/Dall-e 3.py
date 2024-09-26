634c7e0d0fb47b312505e610e011974738c572d4
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 15:54:00 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -6,10 +6,11 @@ st.title("Générateur d'images Dall-e 3")
 OpenAIKey = st.sidebar.text_input("Collez ici votre clé OpenAI :")
 
 client = OpenAI(OpenAIKey)
+prompt_image = st.text_input("Entrez votre prompt ici :")
 
 image = client.images.generate(
     model="dall-e-3",
-    prompt= st.text_input("Entrez votre prompt ici :"),
+    prompt= prompt_image,
     size="512x512",
     quality="standard",
     n=1,

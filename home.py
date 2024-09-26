09a57051c00171260eb9741bd0395098f17f2e5a
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 14:29:14 +0200

    Update home.py

--- home.py
+++ home.py
@@ -14,5 +14,5 @@ st.write(user_input)
 st.image("https://tempo.cdn.tambourine.com/windsong/media/bmot-website-homepage-header-desktop-65f9c01c2da38.mp4")
 
 # Sidebar
-st.sidebar.title
 st.sidebar.page_link("home.py", label="Home")
+st.sidebar.title

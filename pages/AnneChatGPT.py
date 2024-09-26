ebdad102d2e67936707e62969032c2e2ab244b42
annechamallow <anne-charlotte.raimbault@eemi.com>
2024-09-26 16:41:40 +0200

    Update AnneChatGPT.py

--- pages/AnneChatGPT.py
+++ pages/AnneChatGPT.py
@@ -9,24 +9,24 @@ openai_key = st.sidebar.text_input("Collez ici votre clé OpenAI :", '')
 client = OpenAI(api_key = openai_key)
 
 chat_completions = client.chat.completions.create(
-messages= {
-            "role": "system",
-            "content": f"Tu réponds aux questions des utilisateurs",
-            },
-            { 
-            "role": "assistant",
-            "content": "Remplis le prompt",
-            },
-            {
-            "role": "user",
-            "content": prompt ,
-            },
-model="gpt-3.5-turbo",
-temperature=0.3,
-max_tokens=100,
-top_p=1.0,
-frequency_penalty=0.0,
-presence_penalty=0.0,
+            messages= {
+                        "role": "system",
+                        "content": f"Tu réponds aux questions des utilisateurs",
+                        },
+                        { 
+                        "role": "assistant",
+                        "content": "Remplis le prompt",
+                        },
+                        {
+                        "role": "user",
+                        "content": prompt ,
+                        },
+            model="gpt-3.5-turbo",
+            temperature=0.3,
+            max_tokens=100,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
 )
 
 st.write(chat_completions.choices[0].messages.content)
